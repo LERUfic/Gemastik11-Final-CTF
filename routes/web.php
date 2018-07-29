b85ab971869e6e46f9e93a3c6e4526cfbec2e7f1
Unknown <aguelsw@gmail.com>
2018-07-29 10:30:36 +0700

    beautify routing

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@
 
 Route::get('/',function(){ return view('home'); })->name('home');
 
+Route::get('/error', 'HomeController@viewError')->name('error');
+
+Route::get('/logout', 'LoginController@doLogout')->name('login.logout');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 //Auth
 Route::middleware(['guest'])->group(function (){
     Route::get('/login', 'LoginController@viewLogin')->name('login.form');
@@ -20,7 +26,6 @@ Route::middleware(['guest'])->group(function (){
 });
 
 Route::middleware(['auth'])->group(function (){
-	
 	//Admin
 	Route::middleware(['contestant'])->group(function (){
 		Route::prefix('admin')->group(function () {
@@ -31,6 +36,7 @@ Route::middleware(['auth'])->group(function (){
 			Route::get('/register', 'LoginController@viewRegister')->name('register.view');
 			Route::post('/register', 'LoginController@submitRegister')->name('register.submit');
 
+			//Taruh Flag
 			Route::get('/addflag', 'FlagController@viewCreateFlag')->name('flag.create');
 			Route::post('/addflag', 'FlagController@submitCreateFlag')->name('flag.createin');
 
@@ -38,23 +44,17 @@ Route::middleware(['auth'])->group(function (){
 			Route::get('/soal', 'FlagController@viewSoalForm')->name('soal.form');
 			Route::post('/soal', 'FlagController@submitSoal')->name('soal.submit');
     	});
+
+    	Route::get('/scoreboard', 'FlagController@getAllPoin')->name('flag.allscore');
 	});
 
 	//Peserta
 	Route::prefix('peserta')->group(function () {
 		Route::get('/dashboard', 'HomeController@viewDashboardPeserta')->name('peserta.dashboard');
-		Route::get('/permainan', 'FlagController@viewFormSubmit')->name('flag.game');
-		Route::post('/permainan', 'FlagController@submitYourFlag')->name('flag.gamein');
+		Route::get('/game', 'FlagController@viewFormSubmit')->name('flag.game');
+		Route::post('/game', 'FlagController@submitYourFlag')->name('flag.gamein');
 
-		Route::get('/myscore', 'FlagController@getAllPoin')->name('flag.score');
+		Route::get('/myscore', 'FlagController@getPoin')->name('flag.myscore');
 
     });
 });
-
-
-
-Route::get('/error', 'HomeController@viewError')->name('error');
-
-Route::get('/logout', 'LoginController@doLogout')->name('login.logout');
-
-Route::get('/home', 'HomeController@index')->name('home');
